11c72ba2d08ac0b1cf76e1a7a19657da1681262d
Jappananny <Jappananny@gmail.com>
2022-10-02 19:59:35 +0300

    ДЗ4-1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        //Задача 1
+        System.out.println("Задача 1");
+
+        int clientOS = 12;
+        if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+
 
     }
 }
