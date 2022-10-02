3163838d000dde6a1fce5471df4c3ead0c3efe6e
Jappananny <Jappananny@gmail.com>
2022-10-02 22:19:50 +0300

    ДЗ4-5

--- src/Main.java
+++ src/Main.java
@@ -35,5 +35,62 @@ public class Main {
             day = addDays + day;
         } System.out.println("Потребуется дней: " + day);
 
+        //Задача 5
+        System.out.println("Задача 5");
+
+        int monthNumber = 12;
+        switch (monthNumber){
+        case 1:
+        System.out.println("Январь");
+        System.out.println("Зима");
+        break;
+        case 2:
+        System.out.println("Февраль");
+        System.out.println("Зима");
+        break;
+        case 3:
+        System.out.println("Март");
+        System.out.println("Весна");
+        break;
+        case 4:
+        System.out.println("Апрель");
+        System.out.println("Весна");
+        break;
+        case 5:
+        System.out.println("Май");
+        System.out.println("Весна");
+        break;
+        case 6:
+        System.out.println("Июнь");
+        System.out.println("Лето");
+        break;
+        case 7:
+        System.out.println("Июль");
+        System.out.println("Лето");
+        break;
+        case 8:
+        System.out.println("Август");
+        System.out.println("Лето");
+        break;
+        case 9:
+        System.out.println("Сентябрь");
+        System.out.println("Зима близко");
+        break;
+        case 10:
+        System.out.println("Октябрь");
+        System.out.println("Зима близко");
+        break;
+        case 11:
+        System.out.println("Ноябрь");
+        System.out.println("Зима близко");
+        break;
+        case 12:
+        System.out.println("Декабрь");
+        System.out.println("Зима");
+        break;
+        default:
+        System.out.println("Что за месяц");
+}
+
     }
 }
